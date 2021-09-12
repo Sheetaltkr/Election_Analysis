3d3fccefe7f67006aace33288eea7b71b5b81ede
PO <sheetal.tondwalkar@gmail.com>
2021-09-12 00:53:29 -0700

    Election_Analysis python code

---
+++ PyPoll.py
@@ -0,0 +1,66 @@
+# The Data we need to retrieve
+# Total number of votes cast
+# A complete list of candidates who received votes
+# Total number of votes each candidate received
+#Percentage of votes each candidate won
+# The winner of the election based on popular vote
+
+import csv
+import os
+
+
+#with open('Resources/election_results.csv') as election_data:
+#    print(election_data)
+
+#file_to_load = os.path.join("Resources","election_results.csv")
+
+#with open(file_to_load) as election_data:
+#   print(election_data)
+
+
+# Create a filename variable to a direct or indirect path to the file.
+#file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+#outfile= open(file_to_save, "w")
+
+# Write some data to the file.
+#outfile.write("Hello World")
+
+# Close the file
+#outfile.close()
+
+# Using the with statement open the file as a text file.
+#with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+   # txt_file.write("Hello World")
+
+        # Write three counties to the file.
+   # txt_file.write("Arapahoe\nDenver\nJefferson")
+
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here
+      # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+        # Print each row in the CSV file.
+    #for row in file_reader:
+        #print(row[0])
+        #for i in range(len(row)):
+
+            #ccprint(i )
+
+     # Print the header row.
+    headers = next(file_reader)
+    print(headers)
